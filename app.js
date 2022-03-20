274643f1f694b8eec250f2e85ab74b8d5c29d0cc
SurajShukla01 <80586292+SurajShukla01@users.noreply.github.com>
2022-03-20 23:27:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.listen(3000, function(){
+  console.log("Server started at port 3000");
+});
